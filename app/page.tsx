52a7138d146902bddd39722ae469c8aaed5bd882
Sterlyn <sterlyntang31@gmail.com>
2024-06-04 11:53:03 -0400

    Feat: footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Process from "@/components/Process";
@@ -9,7 +10,7 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -18,6 +19,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Process />
+        <Footer />
       </div>
     </main>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50"
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="lg:max-w-[45vw] heading">
+          Ready to take{" "}
+          <span className="text-purple">
+            your digital presence to the next level?
+          </span>
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let&apos;s discuss how you I can help you
+          achieve your goals.
+        </p>
+        <a href="mailto:sterlyntang31@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Sterlyn
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <div
+              key={profile.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg backdrop-filter saturate-180 bg-opacity-75 bg-black-200 rounded-lg border-black-300 border"
+            >
+              <img src={profile.img} alt={profile.id} width={20} height={20} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
